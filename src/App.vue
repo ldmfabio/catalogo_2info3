c8efdcefec7944d30baccee9ed2c59b4ab20fc36
ldmfabio <ldmfabio@gmail.com>
2026-05-05 10:44:05 -0300

    implement price correction functionality and update ProdutoChild component to emit event

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,21 @@
     { id: 8, nome: 'Coleira Azul', preco: 28, categoria: 'Acessórios' },
     { id: 9, nome: 'Guia de Passeio', preco: 40, categoria: 'Acessórios' }
   ])
+
+  const preco = ref(0);
+  const posicaoProduto = ref(-1);
+  const alterando = ref(false);
+
+  function corrigirPreco(idProduto) {
+    posicaoProduto.value = produtos.value.findIndex(p => p.id === idProduto);
+    preco.value = produtos.value[posicaoProduto.value].preco;
+    alterando.value = true;
+  }
+
+  function salvarPreco() {
+    produtos.value[posicaoProduto.value].preco = preco.value;
+    alterando.value = false;
+  }
 </script>
 
 <template>
@@ -20,14 +35,13 @@
     <ul>
       <ProdutoChild v-for="produto in produtos" :key="produto.id"
         :id="produto.id" :nome="produto.nome" :preco="produto.preco"
-        :categoria="produto.categoria">
-
+        :categoria="produto.categoria" @corrigirpreco="corrigirPreco">
       </ProdutoChild>
     </ul>
-    <div v-show="true">
+    <div v-show="alterando">
       <label>Preço</label>
-      <input type="number" v-model="preco">
-      <ButtonChild @clique="corrigirPreco">Salvar</ButtonChild>
+      <input type="number" v-model.number="preco">
+      <ButtonChild @clique="salvarPreco">Salvar</ButtonChild>
     </div>
   </div>
 </template>
--- src/components/ProdutoChild.vue
+++ src/components/ProdutoChild.vue
@@ -1,15 +1,13 @@
 <script setup>
   import ButtonChild from './ButtonChild.vue';
   defineProps(['id', 'nome', 'preco', 'categoria'])
-  function teste() {
-    alert('Opa!')
-  }
+  defineEmits(['corrigirpreco'])
 </script>
 
 <template>
   <li>
     ID: {{ id }} - {{ nome }} - R$ {{ preco }} ({{ categoria }})
-    <ButtonChild @clique="teste">Corrigir Preço</ButtonChild>
+    <ButtonChild @clique="$emit('corrigirpreco', id)">Corrigir Preço</ButtonChild>
   </li>
 </template>
 
